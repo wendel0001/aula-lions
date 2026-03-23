4af3a829b03d0219bba1f580d2849a691ed66b2c
wendel0001 <wendelsauterjj@gmail.com>
2026-03-23 11:09:39 -0300

    update wendel

--- listaarraywendel/estacao.js
+++ listaarraywendel/estacao.js
@@ -10,15 +10,17 @@ estacao.temperatura.push(Number(prompt("digite a segunda temperatura:   ")));
 estacao.temperatura.push(Number(prompt("digite a terceira temperatura:  ")));
 
 
- function media(temperatura) {
-     return (temperatura[0]+temperatura[1]+temperatura[2])/3;
- } 
+const media =(temperatura) => {
+  return (temperatura[0] + temperatura[1] + temperatura[2])/3
+  
+} 
+
  
 if (media(estacao.temperatura) >= 35) {
         estacao.alerta===true
-        console.log("perigo: media de temperatura extrema  "+ media(estacao.temperatura)+ " detectada no" +estacao.local);
+        console.log("perigo: media de temperatura extrema  "+ media(estacao.temperatura) + " detectada no" +estacao.local);
 }else {
     estacao.alerta === false
     console.log("temperaturas dentro da normalidade");
 }
-console.log(estacao);
+console.table(estacao);
